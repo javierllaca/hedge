b28188696c50654e9c3d90dbfe6ebd6fbf113175
Javier Llaca <jl3960@columbia.edu>
2014-06-10 14:36:23 -0400

    Updated xml_to_html.py to improve formatting of html output.

--- scripts/xml_to_html.py
+++ scripts/xml_to_html.py
@@ -5,11 +5,15 @@ if len(sys.argv) != 2:
 	exit()
 
 input_file = open(sys.argv[1], 'r')
-output_file = open(sys.argv[1].replace('.xml', '') + '.html', 'w')
+output_file = open(sys.argv[1].replace('xml/', 'html/').replace('.xml', '.html'), 'w')
 
-output_file.write("<!DOCTYPE html>")
-output_file.write("<html>")
-output_file.write("<meta charset=\"utf-8\">")
+output_file.write("<!DOCTYPE html>\n")
+output_file.write("<html>\n")
+
+output_file.write("<head>\n");
+output_file.write("\t<meta charset=\"utf-8\">\n")
+output_file.write("\t<style type=\"text/css\"> .quote { margin-left:50px; background-color:#DCDCDC } </style>\n")
+output_file.write("</head>\n");
 
 # post number counter
 count = 0
@@ -18,11 +22,11 @@ for line in input_file:
 	if "<doc" in line:
 		output_file.write("<body>\n")
 	elif "</doc" in line:
-		output_file.write("</body>\n")
+		output_file.write("\n</body>\n")
 	elif "<headline" in line:
-		output_file.write("\n<h1>\nTema: \n")
+		output_file.write("\n<h1>\n")
 	elif "</headline" in line:
-		output_file.write("</h1>\n")
+		output_file.write("\n</h1>\n")
 	elif "<post" in line:
 		count += 1
 		tokens = line.split('=')
@@ -31,10 +35,18 @@ for line in input_file:
 		date = tokens[2].replace('id', '').strip()
 
 		output_file.write("\n<h2> %d. %s (%s) </h2>\n" % (count, author, date))
-		output_file.write("<p>\n")
+		output_file.write("<div>\n")
 	elif "</post" in line:
-		output_file.write("</p>\n")
+		output_file.write("\n</div>\n")
+	elif "<quote" in line:
+		output_file.write("\t<div class=\"quote\"><u>Quote:</u>\n")
+	elif "</quote" in line:
+		output_file.write("\t</div>\n")
+	elif "<?xml" in line:
+		output_file.write("\n")
+	elif line[0] == '\n':
+		output_file.write("\n")
 	else:
-		output_file.write(line)
+		output_file.write("\t<p>" + line + "\t</p>")
 
 output_file.write("</html>\n")
