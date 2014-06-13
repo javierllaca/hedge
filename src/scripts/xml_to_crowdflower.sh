08ff5fa517c4e6c65943c0af046151f868cea621
Javier Llaca <jl3960@columbia.edu>
2014-06-13 18:20:35 -0400

    Updated xml_to_crowdflower.sh to take care of compilation if necessary.

--- src/scripts/xml_to_crowdflower.sh
+++ src/scripts/xml_to_crowdflower.sh
@@ -6,10 +6,15 @@ then
 	# mainly for security purposes
 	if [ ! -f $2 ]
 	then
+		if [ ! -f detector/HedgeDetector.class ]
+		then
+			javac -encoding utf8 detector/HedgeDetector.java
+		fi
+
 		python scripts/xml_to_csv.py $1 | java -Dfile.encoding=utf-8 detector/HedgeDetector ../cues/draft.txt | python scripts/purge_csv.py > $2
 
 	else
-		echo "file already exists"
+		echo "output file already exists"
 	fi
 else
 	echo "usage: $0 <input_file> <output_file>"
