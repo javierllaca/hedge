72e33e0bc95b2e7112565c008fe302b22166aff1
Javier Llaca <jl3960@columbia.edu>
2014-06-13 18:19:38 -0400

    Wrote comments and documentation in HedgeDetector.java

--- src/detector/HedgeDetector.java
+++ src/detector/HedgeDetector.java
@@ -9,6 +9,10 @@ import java.util.regex.Matcher;
 import java.text.Normalizer;
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Detects and tags potential hedge cues in text from stdin
+ * @author Javier Llaca
+ */
 public class HedgeDetector
 {
 	public static ArrayList<String> cues;
@@ -16,25 +20,23 @@ public class HedgeDetector
 	public static Matcher matcher;
 	public static int hedgeCount = 0;
 
-	// Loads hedge cues in file into an ArrayList object
+	/** 
+	 * Loads hedge cues in file into an ArrayList object
+	 * @param filename Path to hedge cue file
+	 */
 	public static void loadCues(String filename)
 	{
 		try {
-			File words = new File(filename);
-			Scanner db = new Scanner(words);
+			File cueFile = new File(filename);
+			Scanner db = new Scanner(cueFile);
 
 			cues = new ArrayList<String>();
 			while (db.hasNextLine()) {
 				String line = db.nextLine().trim();
-				if (!line.isEmpty()) {// ignore blank lines
+				if (!line.isEmpty()) {
 					cues.add(line);
-					
-					// If cue has an acute accent
-					// add non-accented version to list
 					if (containsAcuteAccent(line))
-						cues.add(Normalizer.normalize(line, 
-							Normalizer.Form.NFD).replaceAll(
-							"[^\\p{ASCII}]", ""));
+						cues.add(normalized(line));
 				}
 			}
 
@@ -42,14 +44,29 @@ public class HedgeDetector
 		} catch (FileNotFoundException e) {};
 	}
 
-	public static boolean containsAcuteAccent(String s)
+	/**
+	 * Returns copy of str with normalized non-ASCII characters
+	 * @param str Input string
+	 */
+	public static String normalized(String str)
+	{
+		return Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+	}
+
+	/**
+	 * Determines whether str contains characters with acute accent
+	 * @param str Input string
+	 */
+	public static boolean containsAcuteAccent(String str)
 	{
 		Pattern pattern = Pattern.compile("[ÁÉÍÓÚáéíóú]");
-		Matcher matcher = pattern.matcher(s);
+		Matcher matcher = pattern.matcher(str);
 		return matcher.find();
 	}
 
-	// Creates a regular expression pattern from all hedge cues
+	/**
+	 * Creates a regular expression pattern from all hedge cues
+	 */
 	public static void createRegex()
 	{
 		String patternString = "\\b(" + StringUtils.join(cues, "|") + ")\\b";
@@ -59,32 +76,28 @@ public class HedgeDetector
 	public static void main(String[] args)
 	{
 		if (args.length != 1) {
-			System.out.println("Usage: HedgeDetector <hedgeCueFile> <inputFile>");
+			System.out.println("Usage: HedgeDetector <hedgeCueFile>");
 			return;
 		}
 
 		loadCues(args[0]);
 		createRegex();
 
-		//try {
-			//File file = new File(args[1]);
-			Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 
-			while (in.hasNextLine()) {
-				String line = in.nextLine();
-				matcher = pattern.matcher(line);
-				StringBuffer sb = new StringBuffer();
-				while (matcher.find()) {
-					hedgeCount++;
-					matcher.appendReplacement(sb, 
-							"<strong>" + matcher.group() + "</strong>");
-				}
-				matcher.appendTail(sb);
-				System.out.println(sb);
+		while (in.hasNextLine()) {
+			String line = in.nextLine();
+			matcher = pattern.matcher(line);
+			StringBuffer sb = new StringBuffer();
+			while (matcher.find()) {
+				hedgeCount++;
+				matcher.appendReplacement(sb, 
+						"<strong>" + matcher.group() + "</strong>");
 			}
+			matcher.appendTail(sb);
+			System.out.println(sb);
+		}
 
-			//System.out.println("\nHedges: " + hedgeCount);
-			in.close();
-		//} catch (FileNotFoundException e) {};
+		in.close();
 	}
 }
