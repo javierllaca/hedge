869c15be17f9c996b34cdc54aac0814275d46669
Javier Llaca <jl3960@columbia.edu>
2014-07-03 03:58:33 -0400

    Add sentence detection to main driver

--- src/com/javierllaca/hedge/driver/Main.java
+++ src/com/javierllaca/hedge/driver/Main.java
@@ -2,25 +2,37 @@ package com.javierllaca.hedge.driver;
 
 import com.javierllaca.hedge.tag.Tagger;
 import com.javierllaca.hedge.tag.TermNormalizer;
-
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import opennlp.tools.sentdetect.*;
 
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		TermNormalizer normalizer = new TermNormalizer("../database/slang.txt");
 		Tagger tagger = new Tagger(args[0], "strong");
 		Scanner in = new Scanner(System.in);
-		TermNormalizer normalizer = new TermNormalizer("../database/slang.txt");
 
-		// Print csv header
-		System.out.println(in.nextLine());
+		File modelFile = new File("en-sent.bin"); 
+		SentenceModel model = new SentenceModel(modelFile);
+
+		SentenceDetector detector = new SentenceDetectorME(model);
+
+		/* Print csv header */
+		System.out.println("sentence,term,definition1,definition2");
 
 		while (in.hasNextLine()) {
-			ArrayList<String> taggedLines = tagger.tagLine(normalizer.normalizeLine(in.nextLine()));
-			for (String taggedLine : taggedLines)
-				System.out.println(taggedLine);
+			String[] results = detector.sentDetect(in.nextLine());
+			for (String result : results) {
+				ArrayList<String> taggedLines = tagger.tagLine(normalizer.normalizeLine(result));
+				for (String taggedLine : taggedLines) {
+					System.out.println(taggedLine);
+				}
+			}
 		}
+
+		in.close();
 	}
 }
