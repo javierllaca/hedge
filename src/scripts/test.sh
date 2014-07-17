6f0bc2ec4cc513934177818f48e88282f494a6b2
Javier Llaca <jl3960@columbia.edu>
2014-07-17 12:26:37 -0400

    Correct typos in test.sh

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -2,7 +2,7 @@
 
 DRIVER=com/javierllaca/hedge/driver/Main
 HEDGE_FILE=../database/hedges/relational
-INPUT_FILE=~/speech/corpus/399-3/xml #xml #~/speech/deft/deft-spa-DF-386-219549-107253.xml
+INPUT_FILE=~/speech/corpus/399-3/xml
 
 traverse () {
 	if [ -d $1 ]
@@ -16,11 +16,11 @@ traverse () {
 	fi
 }
 
-# Compile
+# Compile java code
 javac -encoding utf8 $DRIVER.java
 
 # Print csv header
 echo "hedge,sentence,usage1,usage2"
 
-# Traverse files in directory -> parse -> tag -> sort (by hedge)
+# Traverse file/directory -> parse -> tag -> sort -> purge
 traverse $INPUT_FILE | python scripts/parse_xml.py | java -Dfile.encoding=utf-8 $DRIVER $HEDGE_FILE | sort | uniq
