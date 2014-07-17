12d2aa2046bcebbcd330c3c2a6a44be66b9ba565
Javier Llaca <jl3960@columbia.edu>
2014-07-17 18:31:44 -0400

    Update main script and rename -- run.sh

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -2,7 +2,7 @@
 
 DRIVER=com/javierllaca/hedge/driver/Main
 HEDGE_FILE=../database/hedges/relational
-INPUT_FILE=~/speech/corpus/399-3/xml
+INPUT_FILE=~/speech/corpus/401
 
 traverse () {
 	if [ -d $1 ]
@@ -20,7 +20,7 @@ traverse () {
 javac -encoding utf8 $DRIVER.java
 
 # Print csv header
-echo "hedge,sentence,usage1,usage2"
+#echo "hedge,sentence,usage1,usage2"
 
 # Traverse file/directory -> parse -> tag -> sort -> purge
 traverse $INPUT_FILE | python scripts/parse_xml.py | java -Dfile.encoding=utf-8 $DRIVER $HEDGE_FILE | sort | uniq
