06bbc3face959269bc8b14a06c4b82fa400d387c
Javier Llaca <jl3960@columbia.edu>
2014-07-21 13:24:49 -0400

    Update java driver and run script

--- src/com/javierllaca/hedge/driver/Main.java
+++ src/com/javierllaca/hedge/driver/Main.java
@@ -7,28 +7,38 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import opennlp.tools.sentdetect.*;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.sentdetect.SentenceDetector;
+import opennlp.tools.sentdetect.SentenceDetectorME;
 
+/**
+ * Driver for hedge detector program
+ * @author Javier Llaca
+ */
 public class Main {
 	public static void main(String[] args) throws Exception {
-		TermNormalizer normalizer = new TermNormalizer("../database/slang");
-		Tagger tagger = new Tagger(args[0], "strong");
-		Scanner in = new Scanner(System.in);
+		if (args.length == 2) {
+			// Setup sentece detection
+			File modelFile = new File("en-sent.bin"); 
+			SentenceModel model = new SentenceModel(modelFile);
+			SentenceDetector detector = new SentenceDetectorME(model);
 
-		File modelFile = new File("en-sent.bin"); 
-		SentenceModel model = new SentenceModel(modelFile);
+			// Setup hedge tagging and text normalization
+			Tagger tagger = new Tagger(args[0], "strong");
+			TermNormalizer normalizer = new TermNormalizer(args[1]);
 
-		SentenceDetector detector = new SentenceDetectorME(model);
+			Scanner in = new Scanner(System.in);
 
-		while (in.hasNextLine()) {
-			String[] results = detector.sentDetect(in.nextLine());
-			for (String result : results) {
-				ArrayList<String> taggedLines = tagger.tagLine(normalizer.normalizeLine(result));
-				for (String taggedLine : taggedLines) {
-					System.out.println(taggedLine);
+			while (in.hasNextLine()) {
+				String[] sentences = detector.sentDetect(in.nextLine());
+				for (String sentence : sentences) {
+					ArrayList<String> tags = tagger.tagLine(normalizer.normalizeLine(sentence));
+					for (String tag : tags) {
+						System.out.println(tag);
+					}
 				}
 			}
+			in.close();
 		}
-		in.close();
 	}
 }
--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+HEDGE_FILE=database/hedges/relational
+SLANG_FILE=database/slang
+
 DRIVER=com/javierllaca/hedge/driver/Main
-HEDGE_FILE=../database/hedges/relational
+
 INPUT_FILE=~/speech/corpus/401
 
+# Recursively traverse a directory by printing file contents
 traverse () {
 	if [ -d $1 ]
 	then
@@ -20,7 +24,7 @@ traverse () {
 javac -encoding utf8 $DRIVER.java
 
 # Print csv header
-#echo "hedge,sentence,usage1,usage2"
+echo "hedge,sentence,usage1,usage2"
 
 # Traverse file/directory -> parse -> tag -> sort -> purge
-traverse $INPUT_FILE | python scripts/parse_xml.py | java -Dfile.encoding=utf-8 $DRIVER $HEDGE_FILE | sort | uniq
+traverse $INPUT_FILE | python scripts/parse_xml.py | java -Dfile.encoding=utf-8 $DRIVER $HEDGE_FILE $SLANG_FILE | sort | uniq
