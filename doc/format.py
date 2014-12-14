caa77a770f348690364b07b82a6c4359201006bd
Javier Llaca <jl3960@columbia.edu>
2014-12-14 09:42:33 -0500

    Update documentation

---
+++ doc/format.py
@@ -0,0 +1,17 @@
+from sys import argv, stdin
+
+def split(s, chars):
+	"""Splits s into lines no longer than a number of characters"""
+	result = tail = ""
+	if len(s) < chars:
+		return s
+	index = s[0:chars].rfind(' ')
+	result += s[0:index + 1] + "\n"
+	tail += s[index + 1:]
+	return result + split(tail, chars)
+
+if __name__ == '__main__':
+	if len(argv) == 2:
+		chars = int(argv[1])
+		for line in stdin:
+			print split(line, chars),
