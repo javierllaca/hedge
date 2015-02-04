2c9ee2244dc0f70732afcb3681b6cbc32707bb43
Javier Llaca <jl3960@columbia.edu>
2015-02-04 02:05:24 -0500

    Add script to compress multiple csv rows

---
+++ src/scripts/compress.py
@@ -0,0 +1,29 @@
+from sys import argv, stdin, stdout
+
+def duplicate(s, bound):
+	res = ""
+	toks = s.split(",")
+	for i in range(bound):
+		res += reduce(lambda a, b: a + "," + b, 
+				map(lambda s: s + "(%d)" % (i + 1), toks)) + ","
+	return res[:-1]
+
+def main(bound):
+	i = 0
+	buf = ""
+	for line in stdin:
+		line = line.strip()
+		# print duplicated header
+		if i == 0:
+			print duplicate(line, bound)
+		# flush buffer
+		elif i % bound == bound - 1:
+			print buf + line
+			buf = ""
+		# compress rows into buffer
+		else:
+			buf += line + ","
+		i += 1
+
+if __name__ == "__main__" and len(argv) == 2:
+	main(int(argv[1]))
